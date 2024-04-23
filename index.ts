e40ff9043a8a13ee154fe0528c86fcc6bb35794a
Haseeb1256 <160749566+Haseeb1256@users.noreply.github.com>
2024-04-23 12:03:54 -0700

    made a guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()* 5 + 1);
+const answer = await inquirer.prompt([
+{
+    name: "userGuessedNumber",
+    type: "number",
+message:"Guess a number between 1-5: ",
+}
+])
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(" Congratulation You guessed the right number");
+} 
+else{
+    console.log("sorry, you guessedthe wrong number");
+}
